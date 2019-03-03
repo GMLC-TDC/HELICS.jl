becb649da9d4a459c5425b408925c1b57ba4e2f4
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-03 05:24:47 +0000

    build based on 650775b

---
+++ dev/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "HELICS.jl",
+    "title": "HELICS.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#HELICS.jl-1",
+    "page": "HELICS.jl",
+    "title": "HELICS.jl",
+    "category": "section",
+    "text": "HELICS.jl is a Julia wrapper for the HELICS package."
+},
+
+{
+    "location": "#Installation-1",
+    "page": "HELICS.jl",
+    "title": "Installation",
+    "category": "section",
+    "text": "For now, this package is registered, so you can use the following to install:Open the package manager REPL (using ])(v1.1) pkg> add HELICSTo install the latest development version, use the following from within Julia:(v1.1) pkg> dev HELICSThis package includes HELICS as a library. You do not have to install HELICS separately.Note that this should work on 32- and 64-bit Windows systems and 64-bit Linux and Mac systems."
+},
+
+]}

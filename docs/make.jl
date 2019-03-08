37c88c35fc46d64a88847262fd0cb41c4ad9f74a
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-03-08 12:07:26 -0700

    Update README.md

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,8 @@
 push!(LOAD_PATH, joinpath(@__DIR__, "../src/"))
 using Documenter, HELICS
 
+cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "./src/index.md"), force=true, follow_symlinks=true)
+
 makedocs(
          sitename="HELICS Julia documentation",
          pages=[

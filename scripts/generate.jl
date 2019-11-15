a7d08040d5441bed659eb667dca3f8d994ea400b
Dheepak Krishnamurthy <me@kdheepak.com>
2019-11-14 21:58:08 -0700

    Update scripts/generate.jl

--- scripts/generate.jl
+++ scripts/generate.jl
@@ -1,14 +1,29 @@
 using Clang
 
 # LIBCLANG_HEADERS are those headers to be wrapped.
-const LIBCLANG_INCLUDE = joinpath(@__DIR__, "../deps/usr/include/helics/shared_api_library") |> normpath
-const LIBCLANG_HEADERS = [joinpath(LIBCLANG_INCLUDE, header) for header in readdir(LIBCLANG_INCLUDE) if endswith(header, ".h")]
+const LIBHELICS_INCLUDE = [
+                           joinpath(@__DIR__, "../deps/usr/include/helics/shared_api_library") |> normpath,
+                           joinpath(@__DIR__, "../deps/usr/include/helics") |> normpath
+                          ]
 
-wc = init(; headers = LIBCLANG_HEADERS,
+const LIBHELICS_HEADERS = String[]
+
+for folder in LIBHELICS_INCLUDE
+    for header in readdir(folder)
+        if endswith(header, ".h")
+            push!(LIBHELICS_HEADERS, joinpath(folder, header))
+        end
+    end
+end
+
+wc = init(; headers = LIBHELICS_HEADERS,
             output_file = joinpath(@__DIR__, "../src/lib.jl"),
             common_file = joinpath(@__DIR__, "../src/common.jl"),
-            clang_includes = vcat(LIBCLANG_INCLUDE, CLANG_INCLUDE),
-            clang_args = ["-I", joinpath(LIBCLANG_INCLUDE, "..")],
+            clang_includes = vcat(LIBHELICS_INCLUDE..., CLANG_INCLUDE),
+            clang_args = [
+                          "-I", joinpath(LIBHELICS_INCLUDE[1], ".."),
+                          "-I", joinpath(LIBHELICS_INCLUDE[2], ".."),
+                         ],
             header_wrapped = (root, current)->root == current,
             header_library = x->"libhelicsSharedLib",
             clang_diagnostics = true,

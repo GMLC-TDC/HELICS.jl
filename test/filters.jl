eed771cecdd3b549cfbc11077676d653dc7fc7bd
Dheepak Krishnamurthy <me@kdheepak.com>
2020-03-05 10:21:22 -0700

    Add info tests for filters

--- test/filters.jl
+++ test/filters.jl
@@ -44,3 +44,48 @@ include("init.jl")
     @test state == h.HELICS_STATE_FINALIZE
 
 end
+
+
+@testset "Filter Type Tests info" begin
+
+    broker = createBroker(2)
+    fFed, fedinfo1 = createMessageFederate(1, "filter")
+    mFed, fedinfo2 = createMessageFederate(1, "message")
+
+    p1 = h.helicsFederateRegisterGlobalEndpoint(mFed, "port1", "")
+    p2 = h.helicsFederateRegisterGlobalEndpoint(mFed, "port2", "")
+
+    h.helicsEndpointSetInfo(p1, "p1_test")
+    h.helicsEndpointSetInfo(p2, "p2_test")
+
+    f1 = h.helicsFederateRegisterFilter(fFed, h.HELICS_FILTER_TYPE_CUSTOM, "filter1")
+    h.helicsFilterAddSourceTarget(f1, "port1")
+    h.helicsFilterSetInfo(f1, "f1_test")
+
+    f2 = h.helicsFederateRegisterFilter(fFed, h.HELICS_FILTER_TYPE_CUSTOM, "filter2")
+    h.helicsFilterAddDestinationTarget(f2, "port2")
+    h.helicsFilterSetInfo(f2, "f2_test")
+
+    ep1 = h.helicsFederateRegisterEndpoint(fFed, "fout", "")
+    h.helicsEndpointSetInfo(ep1, "ep1_test")
+    f3 = h.helicsFederateRegisterFilter(fFed, h.HELICS_FILTER_TYPE_CUSTOM, "c4")
+    h.helicsFilterAddSourceTarget(f3, "filter0/fout")
+    h.helicsFilterSetInfo(f3, "f3_test")
+
+    @test h.helicsEndpointGetInfo(p1) == "p1_test"
+    @test h.helicsEndpointGetInfo(p2) == "p2_test"
+    @test h.helicsEndpointGetInfo(ep1) == "ep1_test"
+
+    @test h.helicsFilterGetInfo(f1) == "f1_test"
+    @test h.helicsFilterGetInfo(f2) == "f2_test"
+    @test h.helicsFilterGetInfo(f3) == "f3_test"
+
+    h.helicsFederateEnterExecutingModeAsync(fFed)
+    h.helicsFederateEnterExecutingMode(mFed)
+    h.helicsFederateEnterExecutingModeComplete(fFed)
+
+    h.helicsFederateFinalizeAsync(mFed)
+    h.helicsFederateFinalize(fFed)
+    h.helicsFederateFinalizeComplete(mFed)
+
+end

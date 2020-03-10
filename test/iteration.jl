a3e0c934f4c5b009931a852d42fd94a34546ab03
Dheepak Krishnamurthy <me@kdheepak.com>
2020-03-10 02:47:55 -0600

    Add more iteration tests

--- test/iteration.jl
+++ test/iteration.jl
@@ -31,3 +31,69 @@ include("init.jl")
     destroyFederate(vFed1, fedinfo)
     destroyBroker(broker)
 end
+
+@testset "Iteration async test" begin
+
+    broker = createBroker(1)
+    vFed1, fedinfo1 = createValueFederate(1, "fed0")
+    vFed2, fedinfo2 = createValueFederate(1, "fed1")
+
+    # register the publications
+    pub1 = h.helicsFederateRegisterGlobalPublication(vFed1, "pub1", h.HELICS_DATA_TYPE_INT)
+
+    sub1 = h.helicsFederateRegisterSubscription(vFed2, "pub1")
+    pub2 = h.helicsFederateRegisterGlobalPublication(vFed2, "pub2", h.HELICS_DATA_TYPE_INT)
+
+    sub2 = h.helicsFederateRegisterSubscription(vFed1, "pub2")
+    h.helicsFederateSetTimeProperty(vFed1, h.HELICS_PROPERTY_TIME_PERIOD, 1.0)
+    h.helicsFederateSetTimeProperty(vFed2, h.HELICS_PROPERTY_TIME_PERIOD, 1.0)
+    # vFed1->setLoggingLevel(5)
+    # vFed2->setLoggingLevel(5)
+
+    h.helicsFederateEnterInitializingModeAsync(vFed1)
+    h.helicsFederateEnterInitializingMode(vFed2)
+    h.helicsFederateEnterInitializingModeComplete(vFed1)
+
+    c1 = 0
+    c2 = 0
+
+    h.helicsPublicationPublishInteger(pub1, c1)
+    h.helicsPublicationPublishInteger(pub2, c2)
+
+    h.helicsFederateEnterExecutingModeAsync(vFed1)
+    h.helicsFederateEnterExecutingMode(vFed2)
+    h.helicsFederateEnterExecutingModeComplete(vFed1)
+
+    while c1 <= 10
+        h.helicsInputGetInteger(sub1), c1
+        h.helicsInputGetInteger(sub2), c2
+        c1 += 1
+        c2 += 1
+
+        if c1 <= 10
+            h.helicsPublicationPublishInteger(pub1, c1)
+            h.helicsPublicationPublishInteger(pub2, c2)
+        end
+
+        h.helicsFederateRequestTimeIterativeAsync(vFed1, 1.0, h.HELICS_ITERATION_REQUEST_ITERATE_IF_NEEDED)
+
+        grantedTime, state = h.helicsFederateRequestTimeIterative(vFed2, 1.0, h.HELICS_ITERATION_REQUEST_ITERATE_IF_NEEDED)
+        if c1 <= 10
+            @test state == h.HELICS_ITERATION_RESULT_ITERATING
+            @test grantedTime == 0.0
+        else
+            @test state == h.HELICS_ITERATION_RESULT_NEXT_STEP
+            @test grantedTime == 1.0
+        end
+
+        grantedTime, state = h.helicsFederateRequestTimeIterativeComplete(vFed1)
+        if c1 <= 10
+            @test state == h.HELICS_ITERATION_RESULT_ITERATING
+            @test grantedTime == 0.0
+        else
+            @test state == h.HELICS_ITERATION_RESULT_NEXT_STEP
+            @test grantedTime == 1.0
+        end
+    end
+
+end
